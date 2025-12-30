822f598d66e9c5598bd71ab70eca80ceb2db8096
Liu <54572905@qq.com>
2025-12-30 11:21:11 +0800

    引用修改对象

--- project-7/src/main.rs
+++ project-7/src/main.rs
@@ -3,6 +3,17 @@ fn main() {
     let mut current_meal: String = String::new();
     add_flour(&mut current_meal);
     show_my_meal(&current_meal);
+
+    // 2. 对象引用可以共存，对象引用且修改完毕之后，才能被再次借用
+    let mut car = String::from("Red");
+    let ref1 = &mut car;    // 引用可修改字符串
+    ref1.push_str( " Add carrot");
+    println!("ref1: {}", ref1);
+    let ref2 = &car;
+
+    // println!("&car: {}", &car);
+    // println!("ref1: {}", ref1);
+    println!("ref2: {}", ref2);
 }
 
 // meal: String
