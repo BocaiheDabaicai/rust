5e4ab1218c5383f70a3d0471421f7e9ea85e6fb3
Liu <54572905@qq.com>
2026-03-30 15:45:17 +0800

    声明模块、模块的简单使用

--- project-14/src/main.rs
+++ project-14/src/main.rs
@@ -1,3 +1,26 @@
+// 1. 创建模块
+mod inventory {
+    const FLOOR_SPACE: i32 = 10000; // 必须声明数据类型
+    pub const MANAGER: &str = "Liu Inventory"; // 不声明公开性，则默认为私有的
+
+    #[derive(Debug)]
+    enum ProductCategory {
+        Ladder,
+        Hammer,
+    }
+
+    struct Item {
+        name: String,
+        category: ProductCategory,
+        quantity: u32,
+    }
+
+    fn talk_to_manager() {
+        println!("That is your coffee? Mr.{MANAGER}");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // 2. 公共关键字
+    println!("Hello, world! This is my manager {}", inventory::MANAGER);
 }
